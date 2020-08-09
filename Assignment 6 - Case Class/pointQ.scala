4b12320761d64ccc9a228afc25b26e08d8bb4223
naushikha <naushikha.jayawickrama@gmail.com>
2020-08-09 11:20:31 +0530

    Did slight a slight change to the available methods

--- Assignment 6 - Case Class/pointQ.scala
+++ Assignment 6 - Case Class/pointQ.scala
@@ -6,19 +6,19 @@ object pointQ extends App{
 		def y: Int = b
 		def +(that: Point) = Point(this.x + that.x, this.y + that.y)
 		def move(dx: Int, dy: Int) = Point(this.x + dx, this. y + dy)
+		def invert = Point(this.y, this.x)
+		def distance = (that: Point) => math.sqrt(math.pow(this.x - that.x, 2) + math.pow(this.y - that.y, 2))
 		
 		override def toString = "(" + x + ", " + y + ")"
 	}
 	
-	val invert = (iPt: Point) => Point(iPt.y, iPt.x)
-	val distance = (p1: Point, p2: Point) => math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2))
 	
 		val p1 = Point(2, 5);
 		val p2 = Point(3, 6);
 		printf("Points used in the demonstration are p1(2, 5) and p2(3, 6).");
 		printf("\n\tp1 + p2 => "); print(p1 + p2);
 		printf("\n\tMoving p1 by 1 unit in both x and y directions => "); print(p1.move(1, 1));
-		printf("\n\tInverting p2 => "); print(invert(p2));
-		printf("\n\tDistance between p1 and p2 => "); print(distance(p1, p2));
+		printf("\n\tInverting p2 => "); print(p2.invert);
+		printf("\n\tDistance between p1 and p2 => "); print(p1.distance(p2));
 		printf("\n");
 }
